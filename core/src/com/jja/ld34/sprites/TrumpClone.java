98485f5bc2b2da13f511c4d8dc3ea5c195329bc4
Andrew Strauch <rememberx@gmail.com>
2015-12-12 14:03:57 -0500

    Created TrumpClone

---
+++ core/src/com/jja/ld34/sprites/TrumpClone.java
@@ -0,0 +1,13 @@
+package com.jja.ld34.sprites;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by andrewstrauch on 12/12/15.
+ */
+public class TrumpClone{
+    /*Probably just have this extend the Protagonist class
+     Instead of checking for input handling, maybe just have it randomly generate its own new direction every X seconds,
+     where X is a randNum in a small range.
+    */
+    }
