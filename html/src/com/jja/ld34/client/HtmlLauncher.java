3d0cb0dffee7ba5252f6105d68d10a0400851e50
James Pizzurro <james.pizzurro@it.com>
2015-12-11 20:21:50 -0500

    Initial commit

---
+++ html/src/com/jja/ld34/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jja.ld34.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jja.ld34.Ld34Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Ld34Game();
+        }
+}
